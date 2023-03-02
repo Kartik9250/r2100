7675eccbd5734a338f9cfb3f127ef62b315b16e2
Kartik <Kartikrana9250@gmail.com>
2023-03-02 09:31:08 +0530

    tested new ui, added graph to the r2100 com script

--- r2100_communication/r2100_comm.py
+++ r2100_communication/r2100_comm.py
@@ -1,5 +1,6 @@
 import serial
-
+import matplotlib.pyplot as plt
+  
 class get_Data:
    def __init__(self, ser, request):
       self.ser = ser.write(request)
@@ -15,7 +16,7 @@ class get_Data:
       self.raw_data = []
       for i in range(0, 50):
          self.raw_data.append(ser.read(1).hex())
-      #print("raw data: ",len(data),data)
+      print("raw data: ",len(self.raw_data),self.raw_data)
       return(self.raw_data)
 
    def filtered(self):
@@ -33,7 +34,7 @@ class get_Data:
       for i in range(0,11):
          self.filtered_data.append([self.dist_lsb[i], self.dist_msb[i], self.echo_lsb[i], self.echo_msb[i]])
          
-      #print("filtered data: ", len(self.filtered_data), self.filtered_data)
+      print("filtered data: ", len(self.filtered_data), self.filtered_data)
       return(self.filtered_data)
    
    def decimal(self):
@@ -44,15 +45,28 @@ class get_Data:
             data.append(int(self.filtered_data[i][2*j+1]+self.filtered_data[i][2*j],16))
          self.dec_data.append(data)
 
-      #print("Decimal data: ",len(self.dec_data), self.dec_data)
+      print("Decimal data: ",len(self.dec_data), self.dec_data)
       return(self.dec_data)
 
-   
+    
 if __name__=='__main__':
-      ser = serial.Serial(port='COM6', baudrate=115200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
-      request = (0xde, 0x01, 0x05, 0x59, 0x83)
-      get_data = get_Data(ser, request)
-
-      print(get_data.decimal())
-
+   ser = serial.Serial(port='COM6', baudrate=115200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
+   request = (0xde, 0x01, 0x05, 0x59, 0x83)
+   get_data = get_Data(ser, request)
+   
+   func_data = get_data.decimal()[::-1]
+   x_values = []
+   y_values = []
+   print("1")
+   for i in range (0,11):
+      x_values.append(i+1)
+      y_values.append(func_data[i][0])
+      
+   plt.bar(x_values, y_values)
+   
+   plt.title("R2100")
+   plt.xlabel("Lazers")
+   plt.ylabel("Distance")
+   
+   plt.show()
    

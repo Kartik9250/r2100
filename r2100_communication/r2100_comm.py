f2ec280d2cc3b0be3595a3a4418c1a4d891f4afd
Kartik9250 <kartikrana9250@gmail.com>
2023-03-02 14:53:04 +0530

    Added realtime r2100 graph

--- r2100_communication/r2100_comm.py
+++ r2100_communication/r2100_comm.py
@@ -1,5 +1,7 @@
 import serial
 import matplotlib.pyplot as plt
+
+from matplotlib.animation import FuncAnimation
   
 class get_Data:
    def __init__(self, ser, request):
@@ -16,7 +18,7 @@ class get_Data:
       self.raw_data = []
       for i in range(0, 50):
          self.raw_data.append(ser.read(1).hex())
-      print("raw data: ",len(self.raw_data),self.raw_data)
+      #print("raw data: ",len(self.raw_data),self.raw_data)
       return(self.raw_data)
 
    def filtered(self):
@@ -34,7 +36,7 @@ class get_Data:
       for i in range(0,11):
          self.filtered_data.append([self.dist_lsb[i], self.dist_msb[i], self.echo_lsb[i], self.echo_msb[i]])
          
-      print("filtered data: ", len(self.filtered_data), self.filtered_data)
+      #print("filtered data: ", len(self.filtered_data), self.filtered_data)
       return(self.filtered_data)
    
    def decimal(self):
@@ -45,9 +47,33 @@ class get_Data:
             data.append(int(self.filtered_data[i][2*j+1]+self.filtered_data[i][2*j],16))
          self.dec_data.append(data)
 
-      print("Decimal data: ",len(self.dec_data), self.dec_data)
-      return(self.dec_data)
-
+      #print("Decimal data: ",len(self.dec_data), self.dec_data)
+      return(self.dec_data)      
+   
+def animate(i):
+   get_data = get_Data(ser, request)
+   #print("i is", i)
+   global func_data
+   y1=func_data[0][0]
+   y2=func_data[1][0]
+   y3=func_data[2][0]
+   y4=func_data[3][0]
+   y5=func_data[4][0]
+   y6=func_data[5][0]
+   y7=func_data[6][0]
+   y8=func_data[7][0]
+   y9=func_data[8][0]
+   y10=func_data[9][0]
+   y11=func_data[10][0]
+   if y1&y2&y3&y4&y5&y6&y7&y8&y10&y11 <= 2000:
+      color_val= 'red'
+   else:
+      color_val = 'blue'
+   plt.clf()
+   plt.bar([1,2,3,4,5,6,7,8,9,10,11], [y1,y2,y3,y4,y5,y6,y7,y8,y9,y10,y11], color=color_val)
+   
+ 
+   func_data = get_data.decimal()[::-1]
     
 if __name__=='__main__':
    #for windows you have to install usb to serial driver
@@ -59,19 +85,29 @@ if __name__=='__main__':
    request = (0xde, 0x01, 0x05, 0x59, 0x83)
    get_data = get_Data(ser, request)
    
+   global func_data
    func_data = get_data.decimal()[::-1]
-   x_values = []
-   y_values = []
-   print("1")
-   for i in range (0,11):
-      x_values.append(i+1)
-      y_values.append(func_data[i][0])
-      
-   plt.bar(x_values, y_values)
-   
-   plt.title("R2100")
-   plt.xlabel("Lazers")
-   plt.ylabel("Distance")
-   
+
+   fig = plt.figure(figsize=(8,6))
+   axes = fig.add_subplot(1,1,1)
+
+   plt.title("R2100", color=("blue"))
+   ani = FuncAnimation(fig, animate, interval=200,blit=False)
+
    plt.show()
+
+
+
+   # print("1")
+   # for i in range (0,11):
+   #    x_values.append(i+1)
+   #    y_values.append(func_data[i][0])
+      
+   # plt.bar(x_values, y_values)
+   
+   # plt.title("R2100")
+   # plt.xlabel("Lazers")
+   # plt.ylabel("Distance")
+   
+   # plt.show()
    

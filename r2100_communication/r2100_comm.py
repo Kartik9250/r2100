a58639541389935acea3b825d404d7084c2f0cac
Kartik9250 <kartikrana9250@gmail.com>
2023-03-14 16:33:11 +0530

    fixed loop freeze

--- r2100_communication/r2100_comm.py
+++ r2100_communication/r2100_comm.py
@@ -9,6 +9,7 @@
 
 # request = (0xde, 0x01, 0x05, 0x59, 0x83)
 # get_data = get_Data(ser, request)
+import serial
 
 class get_Data:
    def __init__(self, ser, request):
@@ -25,22 +26,31 @@ class get_Data:
       self.dec_echo = []
 
 
-      self.ser.write(request)
+      
 
    def raw(self):
+      self.ser.write(request)
       self.raw_data = []
-      for i in range(0, 50):
-         self.raw_data.append(self.ser.read(1).hex())
+      i = 0
+      while i < 50:
+         data = self.ser.read(1).hex()
+         if data == '':
+            self.ser.write(request)
+            continue
+         else:
+            self.raw_data.append(data)
+            i += 1
+      #print(self.raw_data)
       #print("raw data: ",len(self.raw_data),self.raw_data)
       return self.raw_data
 
    def filtered(self):
-      self.raw()
-      self.dist_lsb = self.raw_data[4:-2:4]
-      self.dist_msb = self.raw_data[5:-2:4]
+      data = self.raw()
+      self.dist_lsb = data[4:-2:4]
+      self.dist_msb = data[5:-2:4]
 
-      self.echo_lsb = self.raw_data[6:-2:4]
-      self.echo_msb = self.raw_data[7:-2:4]
+      self.echo_lsb = data[6:-2:4]
+      self.echo_msb = data[7:-2:4]
       #print("dist_lsb: ",len(self.dist_lsb), self.dist_lsb)
       #print("dist_msb: ",len(self.dist_msb), self.dist_msb)
       #print("echo_lsb: ",len(self.echo_lsb), self.echo_lsb)
@@ -53,11 +63,11 @@ class get_Data:
       return self.filtered_data
    
    def decimal_all(self):
-      self.filtered()
+      data = self.filtered()
       for i in range(0,11):
          data = []
          for j in range(0,2):
-            data.append(int(self.filtered_data[i][2*j+1]+self.filtered_data[i][2*j],16))
+            data.append(int(data[i][2*j+1]+data[i][2*j],16))
          self.dec_all.append(data)
 
       #print("All decimal data: ",len(self.dec_all), self.dec_all)
@@ -80,4 +90,15 @@ class get_Data:
          self.dec_dist.append(i[1])
       #print("Decimal echo data: ",len(self.dec_echo), self.dec_echo)
       return self.dec_dist
-    
+
+#count = 1
+#while True:
+#   print(count)
+#   ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=0.002, write_timeout=0.002)
+#   request = (0xde, 0x01, 0x05, 0x59, 0x83)
+#   get_data = get_Data(ser, request)
+#   get_data.raw()
+#   
+#
+#   ser.close()
+#   count += 1
